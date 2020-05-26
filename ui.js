4d89bfd2b1b5e1b2ae2ffea5c417d546a5034cfc
selinylmz <54477284+selinylmz@users.noreply.github.com>
2020-05-26 04:20:06 +0300

    films to ui

--- ui.js
+++ ui.js
@@ -7,12 +7,6 @@ UI.prototype.addFilmToUI=function(newFilm){
     <td></td>
     <td></td>
     <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
-  </tr> -->
-  <!-- <tr>
-    <td><img src="" class="img-fluid img-thumbnail"></td>
-    <td></td>
-    <td></td>
-    <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
   </tr> -->*/
   const filmList =document.getElementById("films");
   filmList.innerHTML+=`
